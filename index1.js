5d84fa658bce5f2adeeffc1aad03a668185608c2
Venkatareddykunduru <venkatareddykunduru123@gmail.com>
2024-07-09 00:52:02 +0530

    modified code for appointmentproject for faster code completion fronted

--- index1.js
+++ index1.js
@@ -1,89 +1,65 @@
-document.addEventListener("DOMContentLoaded", displayvegdata);
+document.addEventListener('DOMContentLoaded', getusers);
 
-function handle(event) {
+async function handle(event) {
     event.preventDefault();
+    try{
     const name = event.target.name.value;
-    const price = event.target.price.value;
-    const quantity = event.target.quantity.value;
-    const form = document.querySelector("form");
-
-    const vegdata = {
+    const phone=event.target.phone.value;
+    const email=event.target.email.value;
+    const data = {
         name: name,
-        price: price,
-        quantity: quantity
+        phone:phone,
+        email:email
     };
 
-    // Send data to server
-    axios
-        .post("https://crudcrud.com/api/a57ceb6f5bf546da92a08731e30ecc4a/vegshop", vegdata)
-        .then((response) => {
-            addtolist(response.data);
+    const response=await axios.post(`http://localhost:3000/adduser`, data)
+        
+    addtolist(data);
+        
+        
+    }catch(err){
+        console.log(err);
+    }
+
+    event.target.reset();
+}
+
+function addtolist(data) {
+    const useritem = document.createElement('li');
+    const userlist = document.getElementById('userlist');
+    //name
+    const textNode = document.createTextNode(`${data.name}  ${data.phone}  ${data.email} `);
+    useritem.appendChild(textNode);
+
+    //delete button
+    const deletebtn=document.createElement('button');
+    deletebtn.textContent='Delete';
+    useritem.appendChild(deletebtn);
+
+    userlist.appendChild(useritem);
+
+    deletebtn.addEventListener('click',()=>{
+        axios.delete(`http://localhost:3000/deleteuser/${data.name}`)
+        .then(()=>{
+            userlist.removeChild(useritem);
         })
-        .catch((err) => {
-            console.log("err: in posting data to the server: " + err);
-        });
-
-    form.reset();
-}
-
-function addtolist(vegdata) {
-    const userItem = document.createElement("li");
-    userItem.appendChild(
-        document.createTextNode(
-            `${vegdata.name}  RS:${vegdata.price}  ${vegdata.quantity}KG`
-        )
-    );
-
-    const inputBtn = document.createElement("input");
-    inputBtn.id = "priceupdate";
-    inputBtn.placeholder = "0";
-    userItem.appendChild(inputBtn);
-
-    const buyBtn = document.createElement("button");
-    buyBtn.appendChild(document.createTextNode("Buy"));
-    userItem.appendChild(buyBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.appendChild(document.createTextNode("Delete"));
-    userItem.appendChild(deleteBtn);
-
-    const userList = document.querySelector("ul");
-    userList.appendChild(userItem);
-
-    buyBtn.addEventListener("click", function () {
-        const buyQuantity = inputBtn.value;
-        vegdata.quantity -= buyQuantity;
-
-        axios
-            .put(`https://crudcrud.com/api/a57ceb6f5bf546da92a08731e30ecc4a/vegshop/${vegdata._id}`, vegdata)
-            .then(response => {
-                userItem.childNodes[0].nodeValue = `${vegdata.name}  RS:${vegdata.price}  ${vegdata.quantity}KG`;
-            })
-            .catch(error => console.log(error));
-    });
-
-    deleteBtn.addEventListener("click", function () {
-        axios
-            .delete(`https://crudcrud.com/api/a57ceb6f5bf546da92a08731e30ecc4a/vegshop/${vegdata._id}`)
-            .then(() => {
-                userList.removeChild(userItem);
-            })
-            .catch(error => console.log(error));
     });
 }
 
-function displayvegdata() {
-    axios
-        .get("https://crudcrud.com/api/a57ceb6f5bf546da92a08731e30ecc4a/vegshop")
-        .then((response) => {
+function getusers() {
+    axios.get(`http://localhost:3000/`)
+        .then(response => {
             const data = response.data;
-            const userList = document.querySelector("ul");
-            userList.innerHTML = "";
             data.forEach(element => {
                 addtolist(element);
             });
         })
-        .catch(error => console.log(error));
+        .catch(err => {
+            console.log('Error in fetching data from the server: ', err);
+        });
 }
 
-//document.querySelector("form").addEventListener("submit", handle);
+
+
+
+
